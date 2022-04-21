46729654704b5d2c2b77e5298a7aa23828f9c976
Sam Wallace <sam@sammymayhem.com>
2022-04-20 19:56:25 -0700

    Got it to create a readme properly and all answers show up.

--- index.js
+++ index.js
@@ -1,47 +1,42 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const savedFile = './saved-readme';
-const generateMarkdown = require('./utils/generateMarkdown');
-// TODO: import the generateMarkdown function from utils/generateMarkdown.js
-
-// TODO: Write code to get user input, generate markdown, and save it to a file.
+const savedFile = './saved-readme/sampleREADME.md';
 
 inquirer.prompt([
     {
         type: 'input',
         message: "What is the amazing Title of your project?",
-        name: 'title'
+        name: 'title',
     },
     {
         type: 'input',
         message: "Describe this fantastic project of yours:",
-        name: 'description'
+        name: 'description',
     },
     {
         type: 'input',
         message: "How can I install this phenomenal project:",
-        name: 'install'
+        name: 'install',
     },
     {
         type: 'input',
         message: "Be the awesome person that you are and provide any instructions I need to know for usage:",
-        name: 'usage'
+        name: 'usage',
     },
     {
         type: 'input',
         message: "Would you like other awesome people to contribute to your amazing project?",
-        name: 'colab'
+        name: 'colab',
     },
     {
         type: 'input',
         message: "If you want to add some fun tests for your application, you can do that here! Make sure to include examples on how to run them:",
-        name: 'tests'
+        name: 'tests',
     },
     {
         type: 'input',
         message: "Let me know any other amazing contributors or fantastic assets that require a shout out:",
-        name: 'credits'
+        name: 'credits',
     },
     {
         type: 'checkbox',
@@ -52,9 +47,9 @@ inquirer.prompt([
             "GNU GPLv3 License",
             "Apache License",
             "OpenBSD License",
-            "Rust Licesnse"
+            "Rust Licesnse",
         ],
-        name: 'licesnse'
+        name: 'licesnse',
     },
     {
         type: 'input',
@@ -64,7 +59,57 @@ inquirer.prompt([
     {
         type: 'input',
         message: "What email address should I use to contact your awesomeness?",
-        name: 'useremail'
+        name: 'useremail',
     }
+]).then((answer) => {
+    const genReadme = `
+    # ${answer.title}
+    
+    
+## Description:
+![license](https://img.shields.io/badge/License-${answer.license}-green.svg "License Badge")
+${answer.description}
 
-])
+## Table of Contents: 
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#Usage)
+- [Contributing](#Contributing)
+- [Tests](#Tests)
+- [Credits](#credits)
+- [License](#License)
+- [Questions](#Questions)
+
+## Installation
+${answer.install}
+
+## Usage
+${answer.usage}
+
+## Contributing
+${answer.colab}
+
+## Tests
+${answer.tests}
+
+## Credits
+${answer.credits}
+
+## License
+This project is covered under the following licence: ${answer.license}
+
+
+## Questions
+Below is my GitHub Profile
+- [GitHub Profile](https://github.com/${answer.username})
+- For any further question, reach out to me at: 
+email:${answer.useremail}
+`
+    fs.writeFile(savedFile, genReadme, (err) => {
+        err ? console.log(err) : console.log('You just generated your spectacular README!')
+    });
+
+});
+
+
+
---
+++ saved-readme/sampleREADME.md
@@ -0,0 +1,42 @@
+
+    # adlhjsf
+    
+    ![license](https://img.shields.io/badge/License-undefined-green.svg "License Badge")
+
+## Description:
+lkajshdfkjshdlkjf
+
+## Table of Contents: 
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#Usage)
+- [Contributing](#Contributing)
+- [Tests](#Tests)
+- [Credits](#credits)
+- [License](#License)
+- [Questions](#Questions)
+
+## Installation
+alskdjhfklsajhd
+
+## Usage
+aslkjdhfklashdfkjash
+
+## Contributing
+lskadjhfklashd
+
+## Tests
+alksjdhflkasj
+
+## Credits
+askdjhfk
+
+## License
+This project is covered under the following licence: undefined
+
+
+## Questions
+Below is my GitHub Profile
+- [GitHub Profile](https://github.com/;dsal;dkfja;sl)
+- For any further question, reach out to me at: 
+email:as;lkdjfa;lskj
